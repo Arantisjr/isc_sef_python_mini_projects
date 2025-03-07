3d73f20c5b776c9deff6b5293eec87e5f6131646
Arantis_jr <arantisjunior@gmail.com>
2025-03-07 18:17:14 +0100

    task5

--- TASK 5/number_guessing_game.py
+++ TASK 5/number_guessing_game.py
@@ -1,5 +1,17 @@
-# import random
+import random
+count = 0
+num = random.randint(1,100)
+while 1:
+    users_num = int(input("Guess the number(1 to 100): "))
+    count+=1
+    if users_num > num:
+        print("Too high, try agin")
+    elif users_num < num:
+        print("Too low, try again")
+    elif users_num == num :
+       break  
 
-# num = random.randint(1,100)
-# users_num = int(input("Guess the number(1 to 100): "))
-# while users_numghhhh
+print(f"congratulations! You guesed it in {count - 1} attemps")
+
+
+
